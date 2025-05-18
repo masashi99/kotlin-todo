ebb5376394234aa11f8ea0bfcbcb61ec23cb2d7c
masashi99 <sgmdq601@gmail.com>
2025-05-18 21:42:23 +0900

    add pong response

--- src/main/kotlin/com/example/todo_demo/Router.kt
+++ src/main/kotlin/com/example/todo_demo/Router.kt
@@ -1,10 +1,12 @@
 package com.example.todo_demo
 
+import com.example.todo_demo.handler.Handler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
-import com.example.todo_demo.handler.Handler
+import reactor.core.publisher.Mono
 
 @Configuration
 class Router (private val handler: Handler) {
@@ -12,7 +14,7 @@ class Router (private val handler: Handler) {
     @Bean
     fun hello() = router {
         accept(MediaType.APPLICATION_JSON).nest {
-            GET("/", handler::getJson)
+            GET("/ping", {ServerResponse.ok().body(Mono.just("pong"), String::class.java)})
         }
     }
 }
